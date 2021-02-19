3e8b6a315a55f68f3e3d68327de0d17d7dc7180e
rupinderkaur30-gif <rupi063094@gmail.com>
2021-02-19 17:44:45 -0500

    commit tree 12a20c7f01051766c9590fbc7fb042bd5f9400f5

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,33 @@ require 'pry'
 class String
 
   def sentence?
-    
-  end
+    if self.end_with?(".")
+      return true
+    else
+      false
+    end
+    end
 
   def question?
-
+if self.end_with?("?")
+  return true
+else
+   false
   end
+end
 
   def exclamation?
+    if self.end_with?("!")
+      return true
+    else
+      false 
+     
+    end
+
 
   end
 
   def count_sentences
-
-  end
+    self.split(/[.?!]+/).count
+end
 end
